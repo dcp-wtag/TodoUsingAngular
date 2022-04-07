634de26ebabfb45e819cc4b27a25fbdff5f8dfd5
Dipta Paul <dipta.paul@welldev.io>
2022-04-07 18:54:24 +0600

    header, body, task, task-item component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoTitleComponent } from './components/logo-title/logo-title.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { BodyContainerComponent } from './components/body-container/body-container.component';
+import { CustomButtonComponent } from './components/custom-button/custom-button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoTitleComponent,
+    SearchBarComponent,
+    BodyContainerComponent,
+    CustomButtonComponent,
+    TasksComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/mock-task.ts
@@ -0,0 +1,28 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    createdAt: 'May 5th at 2:30pm',
+    isDone: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting at School',
+    createdAt: 'May 6th at 1:30pm',
+    isDone: true,
+  },
+  {
+    id: 3,
+    text: 'Food Shopping',
+    createdAt: 'May 5th at 2:30pm',
+    isDone: false,
+  },
+  {
+    id: 4,
+    text: 'Doctors Appointment',
+    createdAt: 'May 5th at 2:30pm',
+    isDone: true,
+  },
+];
